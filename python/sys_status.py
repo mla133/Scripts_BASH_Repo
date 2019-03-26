0fe6731be11ea44b73938dc13b3fcc779d2e9501
Matt Allen <matthew.allen@technipfmc.com>
2019-03-26 12:02:19 -0400

    Adding in several modbusTCP python scripts.

---
+++ python/sys_status.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Poll System Status registers 
+
+from pyModbusTCP.client import ModbusClient
+import time
+
+SERVER_HOST = "192.168.181.76"
+SERVER_PORT = 502
+
+c = ModbusClient()
+
+# uncomment this line to see debug message
+#c.debug(True)
+
+# define modbus server host, port
+c.host(SERVER_HOST)
+c.port(SERVER_PORT)
+
+# open or reconnect TCP to server
+if not c.is_open():
+    if not c.open():
+        print("unable to connect to "+SERVER_HOST+":"+str(SERVER_PORT))
+
+while True:
+    # open or reconnect TCP to server
+    if not c.is_open():
+        if not c.open():
+            print("unable to connect to "+SERVER_HOST+":"+str(SERVER_PORT))
+
+
+    # if open() is ok, read coils (modbus function 0x01)
+    if c.is_open():
+        # read System Status(func 02, regs 4096-4101)
+        regs = c.read_discrete_inputs(4096, 6)
+        # if success display registers
+        if regs:
+            print("SYSTEM STATUS" + str(regs))
+
+    # sleep 1s before next polling
+    time.sleep(1)
