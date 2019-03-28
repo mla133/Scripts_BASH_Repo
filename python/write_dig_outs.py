718b376c54accf950b78795a51b431895a26bcd9
Matt Allen <matthew.allen@technipfmc.com>
2019-03-28 09:02:54 -0400

    Adding in some modbusTCP module examples for reading/writing modbus to ALIV.

--- python/write_dig_outs.py
+++ python/write_dig_outs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Poll System Status registers 
+# Toggle Digital Outputs 
 
 from pyModbusTCP.client import ModbusClient
 import time
@@ -28,11 +28,12 @@ if not c.is_open():
     if not c.open():
         print("unable to connect to "+SERVER_HOST+":"+str(SERVER_PORT))
 
-
-# if open() is ok, read coils (modbus function 0x01)
-if c.is_open():
-    # read System Status(func 02, regs 4096-4106)
-    regs = c.read_discrete_inputs(4096, 11)
-    # if success display registers
-    if regs:
-        print("SYSTEM STATUS" + str(regs))
+while True:
+    # if open() is ok, read coils (modbus function 0x01)
+    if c.is_open():
+        # read System Status(func 02, result 4096-4106)
+        result = c.read_holding_registers(2106, 1)
+        # if success display registers
+        if result:
+            print("PI (float): " + str(result))
+            time.sleep(1)
