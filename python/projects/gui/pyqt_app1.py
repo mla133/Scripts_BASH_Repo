b4f1019f7665a56988234d0281f34229a9bcb236
Matt Allen <matthew.allen@technipfmc.com>
2019-04-09 11:20:09 -0400

    Added some style, sizes and positioning to application.  Also had to use multiple lines of self.SetStyleSheet() to python could parse it.

--- python/projects/gui/pyqt_app1.py
+++ python/projects/gui/pyqt_app1.py
@@ -5,10 +5,22 @@ class PyQtApp(QtWidgets.QWidget):
         QtWidgets.QWidget.__init__(self, parent)
         self.setWindowTitle("PyQt Application")
         self.setWindowIcon(QtGui.QIcon("Your/image/file.png"))
+        self.setMinimumWidth(resolution.width() / 3)
+        self.setMinimumHeight(resolution.height() / 1.5)
+        self.setStyleSheet("QWidget {background-color:rgba(0,41,59,255);}")
+        self.setStyleSheet("QScrollBar:horizontal {width: 1px; height: 1px;background-color: rgba(0,41,59,255);}")  
+        self.setStyleSheet("QScrollBar:vertical {width: 1px;height: 1px;background-color: rgba(0,41,59,255);}")
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
+    desktop = QtWidgets.QApplication.desktop()
+    resolution = desktop.availableGeometry()
     myapp = PyQtApp()
+    myapp.setWindowOpacity(0.95)
     myapp.show()
+    myapp.move(resolution.center() - myapp.rect().center())
     sys.exit(app.exec_())
+else:
+    desktop = QtWidgets.QApplication.desktop()
+    resolution = desktop.availableGeometry()
