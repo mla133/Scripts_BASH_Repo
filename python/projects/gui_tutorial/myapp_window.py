3f70295724021027cd5e78fac91e092f14deebd0
Matt Allen <matthew.allen@technipfmc.com>
2019-04-09 13:19:47 -0400

    Fixed issue where it wasn't taking my changes...

---
+++ python/projects/gui_tutorial/myapp_window.py
@@ -0,0 +1,26 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setWindowTitle("My Awesome App")
+        label = QLabel("THIS IS AWESOME!!!")
+        label.setAlignment(Qt.AlignCenter)
+        self.setCentralWidget(label)
+        toolbar = QToolBar("My main toolbar")
+        self.addToolBar(toolbar)
+
+    def onMyToolBarButtonClick(self, s):
+        print("click", s)
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()  # IMPORTANT: Windows are hidden by default.
+
+# Start the event loop
+app.exec()
