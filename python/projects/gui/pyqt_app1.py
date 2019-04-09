002bbfa8f49bcb43328ed0e9f9bd6b79f2136673
Matt Allen <matthew.allen@technipfmc.com>
2019-04-09 11:59:20 -0400

    Added some buttons, still can't get 2nd one to connect to PNG files supplied in text box.

--- python/projects/gui/pyqt_app1.py
+++ python/projects/gui/pyqt_app1.py
@@ -1,4 +1,28 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
+font_but = QtGui.QFont()
+font_but.setFamily("Segoe UI Symbol")
+font_but.setPointSize(10)
+font_but.setWeight(95)
+
+class PushBut1(QtWidgets.QPushButton):
+   
+    def __init__(self, parent=None):
+        super(PushBut1, self).__init__(parent)
+        self.setMouseTracking(True)
+        self.setStyleSheet("margin: 1px; padding: 7px;")
+        self.setStyleSheet("background-color: rgba(1,255,0,100);color: rgba(1,140,0,100);")
+        self.setStyleSheet("border-style: solid;border-radius: 3px; border-width: 0.5px;border-color: rgba(1,140,0,100);")
+   
+    def enterEvent(self, event):
+        self.setStyleSheet("margin: 1px; padding: 7px;")
+        self.setStyleSheet("background- color: rgba(1,140,040,100);color: rgba(1,140,255,100);")
+        self.setStyleSheet("border-style: solid; border-radius: 3px;border-width: 0.5px;border-color: rgba(1,140,140,100);")
+                                                                                                                                          
+    def leaveEvent(self, event):
+        self.setStyleSheet("margin: 1px; padding: 7px;")
+        self.setStyleSheet("background-color: rgba(1,255,0,100);color: rgba(1,140,0,100);")
+        self.setStyleSheet("border-style: solid;border-radius: 3px; border-width: 0.5px;border-color: rgba(1,140,0,100);")
+
 class PyQtApp(QtWidgets.QWidget):
    
     def __init__(self, parent=None):
@@ -10,6 +34,67 @@ class PyQtApp(QtWidgets.QWidget):
         self.setStyleSheet("QWidget {background-color:rgba(0,41,59,255);}")
         self.setStyleSheet("QScrollBar:horizontal {width: 1px; height: 1px;background-color: rgba(0,41,59,255);}")  
         self.setStyleSheet("QScrollBar:vertical {width: 1px;height: 1px;background-color: rgba(0,41,59,255);}")
+
+        self.textf = QtWidgets.QTextEdit(self)
+        self.textf.setPlaceholderText("Results...")
+        self.textf.setStyleSheet("margin: 1px; padding: 7px;")
+        self.textf.setStyleSheet("background-color: rgba(1,255,0,100);color: rgba(1,140,0,100);")
+        self.textf.setStyleSheet("border-style: solid; border-radius: 3px;border-width: 0.5px;border-color: rgba(1,140,0,100);")
+                                 
+        self.but1 = PushBut1(self)
+        self.but1.setText("Text Border - Blue")
+        self.but1.setFixedWidth(150)
+        self.but1.setFont(font_but)
+        self.but2 = PushBut1(self)
+        self.but2.setText("Image")
+        self.but2.setFixedWidth(72)
+        self.but2.setFont(font_but)
+        self.but3 = PushBut1(self)
+        self.but3.setText("")
+        self.but3.setFixedWidth(72)
+        self.but3.setFont(font_but)
+        self.but4 = PushBut1(self)
+        self.but4.setText("")
+        self.but4.setFixedWidth(72)
+        self.but4.setFont(font_but)
+        self.but5 = PushBut1(self)
+        self.but5.setText("")
+        self.but5.setFixedWidth(72)
+        self.but5.setFont(font_but)
+        self.but6 = PushBut1(self)
+        self.but6.setText("")
+        self.but6.setFixedWidth(72)
+        self.but6.setFont(font_but)
+        self.but7 = PushBut1(self)
+        self.but7.setText("")
+        self.but7.setFixedWidth(72)
+        self.but7.setFont(font_but)
+        self.grid1 = QtWidgets.QGridLayout()
+        self.grid1.addWidget(self.textf, 0, 0, 14, 13)
+        self.grid1.addWidget(self.but1, 0, 14, 1, 1)
+        self.grid1.addWidget(self.but2, 1, 14, 1, 1)
+        self.grid1.addWidget(self.but3, 2, 14, 1, 1)
+        self.grid1.addWidget(self.but4, 3, 14, 1, 1)
+        self.grid1.addWidget(self.but5, 4, 14, 1, 1)
+        self.grid1.addWidget(self.but6, 5, 14, 1, 1)
+        self.grid1.addWidget(self.but7, 6, 14, 1, 1)
+        self.grid1.setContentsMargins(7, 7, 7, 7)
+        self.setLayout(self.grid1)        
+
+        self.but1.clicked.connect(self.on_but1)
+        self.but2.clicked.connect(self.on_but2)
+
+    def on_but1(self):
+        self.textf.setStyleSheet("margin: 1px; padding: 7px; background-color: rgba(1,255,217,100); color: rgba(0,190,255,255);")
+        self.textf.setStyleSheet("border-style: solid; border-radius: 3px; border-width: 0.5px; border-color: rgba(0,140,255,255);")
+    
+    def on_but2(self):
+        txt = self.textf.toPlainText()
+        try:
+            img = QtGui.QPixmap(txt)
+            self.lb1.setPixmap(img.scaledToWidth(72, QtCore.Qt.SmoothTransformation))
+        except:
+            pass
 
 if __name__ == "__main__":
     import sys
