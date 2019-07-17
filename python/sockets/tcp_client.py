a298ce76df44f63b3c5c15e0d4e106a4c2a0a9a3
Matt Allen <matthew.allen@technipfmc.com>
2019-07-17 15:36:36 -0400

    Adding in TCP calls to hopefully interface with THMI.

--- python/sockets/tcp_client.py
+++ python/sockets/tcp_client.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 
-import socket
+import sys, socket, json, time
 
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+#TCP_IP = '127.0.0.1'
+TCP_IP = '192.168.180.86' 
+#TCP_PORT = 5005
+TCP_PORT = 6083
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+#MESSAGE = 'GET 4'
+epoch_time = int(time.time())
+print epoch_time
+
+MESSAGE = '{"ts": %d, "seq": 1, "id": "beaglebone", "type": "cmd", "ins": 0, "text":"GET 4"}' % epoch_time
+json_msg = json.dumps(MESSAGE)
+
+print MESSAGE
+print json_msg
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
+s.send(json_msg)
 data = s.recv(BUFFER_SIZE)
 s.close()
 
