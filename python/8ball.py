c39cd08b0965ea8ba7f29f641ef1ead761eaec0d
Matt Allen <matthew.allen@technipfmc.com>
2020-01-15 11:19:41 -0500

    Changed raw_input() to input(), fixed print() function.  This goes from python 2 to python 3 conventions.

--- python/8ball.py
+++ python/8ball.py
@@ -3,6 +3,9 @@
 
 import random
 answers = ["As I see it, yes","It is certain","It is decidedly so","Most likely","Outlook good","Signs point to yes","Without a doubt","Yes","Yes definitely","You may rely on it","Reply hazy, try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
-raw_input("What is your question?\n")
+input("What is your question?\n")
 response = random.choice(answers)
-print response + '\n'
+print (response + '\n')
+
+# Pause at end of script, hit Enter to close
+input("Press ENTER to exit")
