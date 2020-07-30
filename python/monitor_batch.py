039705cb9730086d91304c3aaa1475d545e9f860
Matt Allen <matthew.allen@technipfmc.com>
2020-07-30 10:42:04 -0400

    Added in some spacing to CSV log file output.  Added a check to detect large changes in calculated vs. AL3 calculations.

--- python/monitor_batch.py
+++ python/monitor_batch.py
@@ -40,8 +40,8 @@ update = c.write_single_coil(4096,1)
 AB_result = c.read_input_registers(0, 4)
 print("Allocated Recipe 7..." + str(hex(AB_result[2])))
 
-# 0x0400 SB variant, preset batch to 100-1000 gallons
-RAND_PRESET_VOL  = round(random.uniform(200.0, 500.0), 0)   # Random preset from 100-1000
+# 0x0400 SB variant, preset batch to 200-1000 gallons
+RAND_PRESET_VOL  = round(random.uniform(200.0, 1000.0), 0)   # Random preset from 100-1000
 # Issue Command Data
 command = c.write_multiple_registers(0, [12, 0x0400, 3])
 command = c.write_float(3, [RAND_PRESET_VOL])
@@ -85,7 +85,19 @@ while True:
         add5_curinj = round(int((c.read_input_registers(3652, 1))[0]),0)
         add5_rate   = round(float((c.read_float(4, 3080, 1))[0]),1)
         add5_vol    = round(float((c.read_double(4, 4520, 1))[0]),3)
-        
+
+        # Set/Get some parameters to determine ranges on batch later
+        add_inj_stop_vol    = round(float((c.read_float(3, 4050, 1))[0]), 2)        # Additive Stop Volume (SYS 804)
+        conv_factor         = round(float((c.read_float(3, 4052, 1))[0]), 3)        # Converstion factor (SYS 807)
+        rec_add5_inj_amt    = round(float((c.read_float(3, 10824, 1))[0]), 2)       # Recipe Inj #5 Injection amt (REC 07 - 029)
+        rec_add5_rate       = round(float((c.read_float(3, 10872, 1))[0]), 2)       # Recipe Inj #5 Rate (REC 07 - 030)
+        rec_1st_pct         = round(float((c.read_float(3, 10976, 1))[0]), 2)       # Prd1 Percent of Recipe 7
+        rec_2nd_pct         = round(float((c.read_float(3, 10978, 1))[0]), 2)       # Prd2 Percent of Recipe 7
+
+        pacing_vol = preset_vol - add_inj_stop_vol
+        calc_add_rate = round((rec_add5_rate*pacing_vol)/preset_vol, 1)
+        tdiv = round((pacing_vol/rec_add5_rate)*(rec_add5_inj_amt/conv_factor), 3)
+
         # check for alarms, if any, bail and notify user
         if status[8]:
             print("ALARM occurred...check AccuLoad.")
@@ -99,8 +111,18 @@ while True:
         # log the polling into a CSV file for later (appending)
         with open(LOGFILE, 'a') as inj_data:
             inj_data_writer = csv.writer(inj_data)
-            inj_data_writer.writerow([AL3time[0:11], trans_no, batch_no, recipe_no, preset_vol, trans_GV, add5_curinj, add5_rate, add5_vol])
+            inj_data_writer.writerow([AL3time[0:11], trans_no, batch_no, recipe_no, " ", preset_vol, trans_GV, add5_curinj, " ", add5_rate, calc_add_rate, " ", tdiv, add5_vol])
         inj_data.close()
+
+        # check to see if the calculated additive rate doesn't match with expected rate
+        # and alarm to bail out of testing loops...
+        #if calc_add_rate != add5_rate:
+        if abs(calc_add_rate - add5_rate) > 2.0:
+            alarm = c.write_single_register(2112, 1)
+            print("U1: Add5 Rate Change alarm present")
+            print("Current Additive rate: " + str(add5_rate) + " Calc Additive rate: " + str(calc_add_rate))
+            break
+      
 
     # sleep 0.5s before next polling
     time.sleep(0.5)
@@ -116,30 +138,3 @@ command = c.write_multiple_registers(0, [4, 0x0400, 5])
 update = c.write_single_coil(4096,1)
 ET_result = c.read_input_registers(0, 4)
 print("Ending Transaction..." + str(hex(ET_result[2])))
-
-# Set/Get some parameters to determine ranges on batch later
-add_inj_stop_vol    = float((c.read_float(3, 4050, 1))[0])        # Additive Stop Volume (SYS 804)
-conv_factor         = float((c.read_float(3, 4052, 1))[0])        # Converstion factor (SYS 807)
-rec_add5_inj_amt    = round(float((c.read_float(3, 10824, 1))[0]), 2)       # Recipe Inj #5 Injection amt (REC 07 - 029)
-rec_add5_rate       = round(float((c.read_float(3, 10872, 1))[0]), 2)       # Recipe Inj #5 Rate (REC 07 - 030)
-rec_1st_pct         = round(float((c.read_float(3, 10976, 1))[0]), 2)       # Prd1 Percent of Recipe 7
-rec_2nd_pct         = round(float((c.read_float(3, 10978, 1))[0]), 2)       # Prd2 Percent of Recipe 7
-
-# Total downstream amount = preset_vol * conv_factor * sum(vol_cycle/add_rate)
-
-tdiv = preset_vol * conv_factor * (rec_add5_inj_amt / rec_add5_rate)
-
-print("")
-print("PRESET = " + str(preset_vol))
-print("CONV_FACTOR = " + str(conv_factor))
-print("R5_INJ_AMT = " + str(rec_add5_inj_amt))
-print("R5_RATE = " + str(rec_add5_rate))
-print("TDIV = " + str(tdiv))
-print("ADD5 VOL = " + str(add5_vol))
-
-
-
-
-
-
-
